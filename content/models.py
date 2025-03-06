a680027ed3808ce0a968cd53e9c29c607478eb59
MarekSvec <marek.svec@student.gyarab.cz>
2025-03-06 10:05:38 +0100

    hopeful update

--- content/models.py
+++ content/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.IntegerField()
+    war = models.CharField(max_length=200)
+    description = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    image = models.URLField(max_length=100)
+    imageWar = models.URLField(max_length=100)
+    tactics_link = models.CharField(max_length=100)
+    tactics = models.TextField()
+    imageTactics = models.URLField(max_length=100)
+    context = models.TextField()
+
+    def __str__(self):
+        return self.title[:20]
