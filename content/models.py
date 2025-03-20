427c03a1fc5ba728e55f6e4e58e852f7eb64715e
MarekSvec <marek.svec@student.gyarab.cz>
2025-03-20 10:06:42 +0100

    řazení

--- content/models.py
+++ content/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     year = models.IntegerField()
     war = models.CharField(max_length=200)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
     location = models.CharField(max_length=100)
     casualties = models.IntegerField(default=0)
     image = models.URLField(max_length=100)
@@ -14,6 +14,10 @@ class Article(models.Model):
     tactics = models.TextField()
     imageTactics = models.URLField(max_length=100)
     context = models.TextField()
+    order = models.IntegerField(default=0)
+
+
+
 
     def __str__(self):
         return self.title[:20]
